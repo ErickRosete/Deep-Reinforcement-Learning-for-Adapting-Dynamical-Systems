410293869521f8149c054c9b9bf0a827077f825b
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2021-02-07 21:53:09 +0100

    updated SAC test with hydra

--- Soft_Actor_Critic/sac_test.py
+++ Soft_Actor_Critic/sac_test.py
@@ -1,22 +1,17 @@
 import sys
+import hydra
 from pathlib import Path
-from sac_agent import SAC_Agent
 sys.path.insert(0, str(Path(__file__).parents[1]))
+from Soft_Actor_Critic.sac_agent import SAC_Agent
 from env.sawyer_peg_env import custom_sawyer_peg_env
+from utils.utils import add_cwd
 
-def main():
-    eval_cfg = {"num_episodes": 20, "render": False}
-    env_cfg = {"show_gui": True, "max_episode_steps": 500, "with_force": False, 
-        "with_joint":False, "relative": True, "with_noise":False, "reward_type":"shaped_2",
-        "dt":0.005}
-    agent_cfg = {  "batch_size": 256, "gamma": 0.99, "tau": 0.005, "actor_lr": 3e-4,
-        "critic_lr": 3e-4, "alpha_lr": 3e-4, "hidden_dim": 256}
-    model_name = "sac_peg_v2_pose_2200.pth"
-
-    env = custom_sawyer_peg_env(env_cfg)
-    agent = SAC_Agent(env, **agent_cfg)
-    agent.load(model_name)
-    stats = agent.evaluate(**eval_cfg)
+@hydra.main(config_path="../config", config_name="sac_config")
+def main(cfg):
+    env = custom_sawyer_peg_env(cfg.env)
+    agent = SAC_Agent(env, **cfg.agent)
+    agent.load(add_cwd(cfg.test.model_name))
+    stats = agent.evaluate(**cfg.test)
     print(stats)
     
 if __name__ == "__main__":
